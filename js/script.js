c690eaaba45dccebcaf70c06e920dacfc1e6eec4
AJJackson88 <lilmeer@sbcglobal.net>
2015-10-08 20:28:38 -0700

    Variables
    First step input variables.

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Ameer J. Jackson 10/9/2015 AssignmentName:Expressions
+
+// Expressions - PEMDAS - The Order of Operations
+
+alert("Hello, how may I assist you today?")
+
+// Variables
+var milk;
+var eggs;
+var mayonnaise;
+var kraftsinglescheese;
+var sandwichmeat;
+var aquafinabottlewater;
+var cereal;
+var bread;
+var topramennoodles; 
+var juice;
+var amount;
+var salestax;
+var totalamount
