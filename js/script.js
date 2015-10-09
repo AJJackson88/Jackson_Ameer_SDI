3fa90e6f49c3c9c55ad8cf88a048eb7e046a88a7
AJJackson88 <lilmeer@sbcglobal.net>
2015-10-08 20:31:49 -0700

    Prompts
    Second step input prompts.

--- js/script.js
+++ js/script.js
@@ -18,3 +18,43 @@ var juice;
 var amount;
 var salestax;
 var totalamount
+
+// prompt
+milk = prompt("How many cartons of milk would you like to purchase ($5.00ea.):");
+milk = Number(milk) * 7;
+
+// prompt
+eggs = prompt("How many cartons of eggs would you like to purchase ($6.00ea.):");
+eggs = Number(eggs) * 6;
+
+// prompt
+mayonnaise = prompt("How many mayonnaise would you like to purchase ($4.00ea.):");
+mayonnaise = Number(mayonnaise) * 4;
+
+// prompt
+kraftsinglesslicecheese = prompt("How many packs of Kraft Singles slice cheese would you like to purchase ($3.00ea.):");
+kraftsinglesslicecheese = Number(kraftsinglesslicecheese) * 3;
+
+// prompt
+sandwichmeat = prompt("How many packs of sandwich meat would you like to purchase ($6.00ea.):");
+sandwichmeat = Number(sandwichmeat) * 6;
+
+// prompt
+aquafinabottlewater = prompt("How many cases of Aquafina bottle water would you like to purchase ($6.00ea.):");
+aquafinabottlewater = Number(aquafinabottlewater) * 6;
+
+// prompt
+cereal = prompt("How many boxes of cereal would you like to purchase ($5.00ea.):");
+cereal = Number(cereal) * 5;
+
+// prompt
+bread = prompt("How many loaves of bread would you like to purchase ($1.00ea.):");
+bread = Number(bread) * 1;
+
+// prompt
+topramennoodles = prompt("How many Top Ramen noodles would you like to purchase ($0.17ea.):");
+topramennoodles = Number(topramennoodles) * .17;
+
+// prompt
+juice = prompt("How many juices would you like to purchase ($2.50ea.):");
+juice = Number(juice) * 2.50;
