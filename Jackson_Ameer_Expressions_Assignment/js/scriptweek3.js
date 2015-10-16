e47eb8be50f21ef68b91d79b25275a78b51611f2
AJJackson88 <lilmeer@sbcglobal.net>
2015-10-15 22:07:36 -0700

    Fixed Console Error
    Final final Conditional Assignment after correcting console error.

--- Jackson_Ameer_Expressions_Assignment/js/scriptweek3.js
+++ Jackson_Ameer_Expressions_Assignment/js/scriptweek3.js
@@ -35,12 +35,10 @@ atticaccess = confirm("Is there attic access? (Ok for Yes, Cancel for No)");
 				console.log("The total distance to the phone jack running the wire through attic will be " + extraFeet + "ft.");
 	
 			} else if (extraFeet === 0) {
-				
 				extraFeet = Number(prompt("Please enter the extra feet of wire needed:"));
 				console.log("Thank you. The new total feet of wire needed is " + (extraFeet + distancetojack) + "ft.")
 			
 			} else {
-			
 				// code that runs if there is attic access and extraFeet is less than 10
 				console.log("This route does not require much more feet.");
 }
@@ -49,52 +47,53 @@ atticaccess = confirm("Is there attic access? (Ok for Yes, Cancel for No)");
 
 		// code that runs if there is no attic access
 		console.log("No, there is no attic access.");
-	
 		console.log("It is " + crawlspace + " that there is a crawl space entrence?");
-
+}
 // confirm Boolean of either true or false
 crawlspace = confirm("Is there a crawl space entrence? (Ok for Yes, Cancel for No)");
-	
+
 	if (crawlspace === true) {
 
 		// code that will run if there is a crawl spaces entrence
 		var extraFeet = prompt("How many more feet of wire is needed for this route?");
 		extraFeet = Number(extraFeet);
-
+		
 			// number conditional
 			if (extraFeet >= 10) {
 			
-			// code that will run if there is a crawl space entrence and extarFeet is greater than or eqaul to 10
-			
-			extraFeet = extraFeet + distancetojack;
-			console.log("The total distance to the phone jack running the wire under house will be " + extraFeet + ".")
+				// code that will run if there is a crawl space entrence and extarFeet is greater than or eqaul to 10		
+				extraFeet = extraFeet + distancetojack;
+				console.log("The total distance to the phone jack running the wire under house will be " + extraFeet + ".");
 	
 			} else if (extraFeet === 0) {
 				extraFeet = Number(prompt("Please enter the extra feet of wire needed:"));
-				console.log("Thank you. The new total feet of wire needed is " + (extraFeet + distancetojack) + "ft.")
+				console.log("Thank you. The new total feet of wire needed is " + (extraFeet + distancetojack) + "ft.");
 			
 			} else {
-			
-			// code that runs if there is attic access and extraFeet is less than 10
-			console.log("This route does not require much more feet.");
+				// code that runs if there is attic access and extraFeet is less than 10
+				console.log("This route does not require much more feet.");
 }
-
-	} else {
-	
+ 
+	} else {	
 	// code that will run if there is no crawl space entrence
 	console.log("No, there is no crawl space entrence.");
-	var deny = prompt("Have you looked for an alternative route?");
-			
-			// validation
-			if (deny === "") {
-		
-				// if they entered nothing, confirm denial of service
-				deny = prompt("So you are saying that there are no alternative routes and we have to deny the customer service right?")
-				console.log("Thanks. Please inform the customer that " + deny + ", we have to deny service.")
 	
-			} else {
+		// Validation
+		var deny = prompt("Have you looked for an alternative route?");
+	
+			if (deny === "") {
 
+				// if they entered nothing, confirm denial of service
+				deny = prompt("So you are saying that there are no alternative routes and we have to deny the customer service right?");
+				console.log("Thanks. Please inform the customer that " + deny + ", we have to deny service.");
+
+			} else {
 				//if they entered yes
-				console.log("Ok, Please inform the customer that " + deny + " we have to deny service.")
+				console.log("Ok, Please inform the customer that " + deny + " we have to deny service.");
+
 
 	}
+
+}
+
+
