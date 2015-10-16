5dffc416ecda38d67284d433f634d3415bea8bad
AJJackson88 <lilmeer@sbcglobal.net>
2015-10-15 19:56:19 -0700

    Conditionals: 2nd Boolean
    Second Boolean Conditional (True or False code.)

--- Jackson_Ameer_Expressions_Assignment/js/scriptweek3.js
+++ Jackson_Ameer_Expressions_Assignment/js/scriptweek3.js
@@ -50,4 +50,33 @@ atticaccess = confirm("Is there attic access? (Ok for Yes, Cancel for No)");
 		// code that runs if there is no attic access
 		console.log("No, there is no attic access.");
 	
-		
+		console.log("It is " + crawlspace + " that there is a crawl space entrence?");
+
+// confirm Boolean of either true or false
+crawlspace = confirm("Is there a crawl space entrence? (Ok for Yes, Cancel for No)");
+	
+	if (crawlspace === true) {
+
+		// code that will run if there is a crawl spaces entrence
+		var extraFeet = prompt("How many more feet of wire is needed for this route?");
+		extraFeet = Number(extraFeet);
+
+			// number conditional
+			if (extraFeet >= 10) {
+			
+			// code that will run if there is a crawl space entrence and extarFeet is greater than or eqaul to 10
+			
+			extraFeet = extraFeet + distancetojack;
+			console.log("The total distance to the phone jack running the wire under house will be " + extraFeet + ".")
+	
+			} else if (extraFeet === 0) {
+				extraFeet = Number(prompt("Please enter the extra feet of wire needed:"));
+				console.log("Thank you. The new total feet of wire needed is " + (extraFeet + distancetojack) + "ft.")
+			
+			} else {
+			
+			// code that runs if there is attic access and extraFeet is less than 10
+			console.log("This route does not require much more feet.");
+}
+
+	
