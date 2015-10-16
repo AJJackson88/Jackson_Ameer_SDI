ed89b73347dc3e272c5c83f9e67af1295468c1c3
AJJackson88 <lilmeer@sbcglobal.net>
2015-10-15 19:48:48 -0700

    HTML & Java Script
    2nd HTML file and Java script variables.

---
+++ Jackson_Ameer_Expressions_Assignment/js/scriptweek3.js
@@ -0,0 +1,12 @@
+// Ameer J. Jackson 10/15/2015 AssignmentName:Conditionals
+
+// Conditionals - PEMDAS - The Order of Operations
+
+// variables
+var NID = "\"Network Interface Device\"";
+var jack = "telephone jack";
+var distancetojack = 100;
+var atticaccess = true;
+var crawlspace = true;
+
+
