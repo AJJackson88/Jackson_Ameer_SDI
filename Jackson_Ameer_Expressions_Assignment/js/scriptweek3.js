4f7466c501f5366e8d990d6c3d604cb61bf053ca
AJJackson88 <lilmeer@sbcglobal.net>
2015-10-15 19:51:30 -0700

    Conditional outputs
    Console.log outputs

--- Jackson_Ameer_Expressions_Assignment/js/scriptweek3.js
+++ Jackson_Ameer_Expressions_Assignment/js/scriptweek3.js
@@ -9,4 +9,9 @@ var distancetojack = 100;
 var atticaccess = true;
 var crawlspace = true;
 
+// outputs
+console.log("The start point of my wire run is at the " + NID + " which is located on the outside of the customer's home.");
+console.log("The end point of my wire run is at the " + jack + ", which is located in the customer's office.");
+console.log("The distance is approximately " + distancetojack + "ft. straight from the " + NID + " to the office.");
+console.log("It is " + atticaccess + " that there is attic access?");
 
